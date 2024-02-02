0d4ab94d436853db1baf17053813058c477851a2
Rahul Mondal <rahulmondar@gmail.com>
2024-02-02 10:27:45 +0530

    feat: added cart model in shopping service

--- shopping_service/src/database/models/Cart.ts
+++ shopping_service/src/database/models/Cart.ts
@@ -1,15 +1,10 @@
-import { IOrder } from "@/types";
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema<IOrder>(
+const CartSchema = new Schema(
   {
-    orderId: { type: String, required: true },
     customerId: { type: String, required: true },
-    amount: { type: Number, required: true },
-    status: { type: String, required: true },
-    txnId: { type: String, required: true },
     items: [
       {
         product: {
@@ -30,11 +25,11 @@ const OrderSchema = new Schema<IOrder>(
   {
     timestamps: true,
     toJSON: {
-      transform(_, ret) {
-        delete ret.__v;
+      transform(_doc, ret) {
+        delete ret.__vk;
       },
     },
   },
 );
 
-export default mongoose.model("order", OrderSchema);
+export default mongoose.model("cart", CartSchema);
