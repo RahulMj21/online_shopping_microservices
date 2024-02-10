bffdb28d9bb21d355f6d4774e26ff216be578d5b
Rahul Mondal <rahulmondar@gmail.com>
2024-02-10 12:24:22 +0530

    feat: fixed ngnix.conf file

--- customer_service/src/app.ts
+++ customer_service/src/app.ts
@@ -4,7 +4,6 @@ import helmet from "helmet";
 import cors from "cors";
 import { customerRoutes, healthcheckRoutes } from "@/routes";
 import errorHandler from "@/middleware/errorHandler";
-import { Logger } from "@/utils/logger";
 
 const app = express();
 
@@ -14,12 +13,6 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
 app.use(helmet());
-
-// log path
-app.use((req, _res, next) => {
-  Logger.info(`${req.method} => ${req.path}`);
-  next();
-});
 
 // routes
 app.use("/customer", healthcheckRoutes);
